f0d5f002884280b6b1eb7e720ec960fb2c26d62c
pankajauengg <pankajauengg@gmail.com>
2024-07-09 21:37:12 +0530

    Update ota.php

--- ota.php
+++ ota.php
@@ -1,8 +1,8 @@
 {
 "TXV":"V3.0.1",
-"TXFile":"V3.0.1",
+"TXFile":"https://github.com/pankajauengg/otaSecret/raw/main/TXFirmware.bin",
 "RXV":"V3.0.1",
 "RXFile":"V3.0.1",
-"ASIV":"V1.0.1",
+"ASIV":"V0.3",
 "ASIFile":"V3.0.1"
 }
